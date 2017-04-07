edf74c745da5039f566fb1546acd976a0c4e36f7
menty <1132760021@qq.com>
2017-04-07 18:12:35 +0800

    log responseContent，并重新组装responseBody

--- app/src/main/java/com/retrofit_test/http/LoggingIntercepror.java
+++ app/src/main/java/com/retrofit_test/http/LoggingIntercepror.java
@@ -5,8 +5,10 @@ import com.retrofit_test.util.Logger;
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Created by 黄海 on 2017/1/23.
@@ -18,8 +20,11 @@ public class LoggingIntercepror implements Interceptor {
         Request request = chain.request();
         Logger.i(String.format(request.method() + ",发送请求:%s on %s %s", request.url(), request.headers(), chain.connection()));
         Response response = chain.proceed(request);
-        Logger.i("响应结果:", "code=" + response.code() + " || isSuccessful=" + response.isSuccessful() + " || message=" + response.message());
-//        Logger.i("body="+response.body().string());
-        return response;
-        }
-        }
+
+        MediaType contentType = response.body().contentType();
+        String responseBody = response.body().string();
+        Logger.i("响应结果:", "code=" + response.code() + " || isSuccessful=" + response.isSuccessful() + " || message=" + response.message() + "\n|| responseBody=" + responseBody);
+        ResponseBody body = ResponseBody.create(contentType, responseBody);
+        return response.newBuilder().body(body).build();
+    }
+}
